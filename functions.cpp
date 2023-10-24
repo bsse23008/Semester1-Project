622ef8dec4811d97c87131f27b88ce0750b9c899
Shuja ud Din <bsse23035@itu.edu.pk>
2023-10-24 16:40:13 +0500

    Initial commit

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+#include "functions.h"
+#include <iostream>
+
+using namespace std;
+
+//  <--- ** SignUp Functionality here.... ** -->
+
+
+
+
+
+//  <--- ** Login Functionality here.... ** --->
+
+
+
+
+
+//   <--- ** Admin Controls Section ** --->
+
+
+
+
+
+
+//   <--- ** User Controls Section ** --->
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+int main() {
+
+    //Call your functions here
+
+   
+    return 0;
+}
